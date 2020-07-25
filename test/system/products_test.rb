7983589e30124e3730ebd49225adbf30de8e4acd
richard jarram <rjarram@me.com>
2020-07-25 16:06:13 -0400

    built system test

---
+++ test/system/products_test.rb
@@ -0,0 +1,12 @@
+require "application_system_test_case"
+
+class ProductsTest < ApplicationSystemTestCase
+  # 2. test exercise setup
+  test 'visiting the index page works' do
+    visit root_path
+    # use launchy to show the test results from headless chrome
+    save_and_open_screenshot
+    # 3. verfiy
+    assert_selector 'h1', text: 'Awesome Products'
+  end
+end
